b378f07b20e1b153ca3c2b54c671f4da2d2c6c17
yukunxjr <yukunxjr@yahoo.co.jp>
2022-09-29 09:48:26 +0900

    リファクタリング

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,17 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-    before do
-        @user = FactoryBot.create(:user)
-        @project = FactoryBot.create(:project, owner: @user)
-        @task = @project.tasks.create!(name: "Test task")
-    end
+    include_context "project setup"
 
     describe "show" do
         # JSON 形式でレスポンスを返すこと
         it "responds with JSON formatted output" do
-            sign_in @user
-            get :show,format: :json,params: { project_id: @project.id, id: @task.id }
+            sign_in user
+            get :show,format: :json,params: { project_id: project.id, id: task.id }
             expect(response.content_type).to include "application/json"
         end
     end
@@ -20,17 +16,17 @@ RSpec.describe TasksController, type: :controller do
         # JSON 形式でレスポンスを返すこと
         it "responds with JSON formatted output" do
             new_task = { name: "New Task" }
-            sign_in @user
-            post :create,format: :json,params: { project_id: @project.id, task: new_task }
+            sign_in user
+            post :create,format: :json,params: { project_id: project.id, task: new_task }
             expect(response.content_type).to include "application/json"
         end
         # 新しいタスクをプロジェクトに追加すること
         it "adds a new task to the project" do
             new_task = { name: "New task" }
-            sign_in @user
+            sign_in user
             expect{
-                post :create, format: :json,params: { project_id: @project.id, task: new_task }
-            }.to change(@project.tasks, :count).by(1)
+                post :create, format: :json,params: { project_id: project.id, task: new_task }
+            }.to change(project.tasks, :count).by(1)
         end
     end
 end
